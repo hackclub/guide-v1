f1baa3526d5aebdb241e6031df281acb3bfa10bd
Mingjie Jiang <jiang@mingjie.info>
2019-04-10 18:08:54 -0400

    Add feedback form

--- docs/_assets/js/footer-inject.js
+++ docs/_assets/js/footer-inject.js
@@ -28,6 +28,20 @@ function install(hook, vm) {
 
   hook.afterEach(function(html) {
     var editLink =
+      `
+      <div class="wrap-collabsible" id="feedback-form">
+        <input id="collapsible" class="toggle" type="checkbox">
+        <label for="collapsible" class="lbl-toggle">Finding this guide helpful? Help us improve by sharing your thoughts!</label>
+        <div class="collapsible-content">
+          <div class="content-inner">
+            <textarea name="feedback" id="feedback" class="feedback" placeholder="I want to see information about ..." /></textarea>
+            <div style="text-align: center">
+              <a class="btn submit" id="feedback-submit" onclick="submitFeedback()">Submit</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      ` +
       '<p class="' +
       cssClass +
       '">' +
@@ -45,3 +59,28 @@ function install(hook, vm) {
 }
 
 $docsify.plugins = [].concat(install, $docsify.plugins);
+
+/** Ajax form Submission */
+function submitFeedback() {
+  if (document.getElementById("feedback").value != "") {
+    var xhr = new XMLHttpRequest();
+    var url = "https://usebasin.com/f/be3a90fcc3dd.json";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.feedback + ", " + json.pageUrl);
+        document.getElementById("feedback-form").innerHTML = `
+          <h4 style="color: #d3d3d3">Thanks for your feedback! We‘ll keep improving this leadership guide. In the mean time, if you have more questions or suggestions, 
+          feel free to talk to us in <a href="https://hackclub.slack.com/messages/C0C78SG9L/">#hq</a>!</h4>
+        `;
+      }
+    };
+    var data = JSON.stringify({
+      feedback: document.getElementById("feedback").value,
+      pageUrl: window.location.hash
+    });
+    xhr.send(data);
+  }
+}
