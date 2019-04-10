22659b5af383a40baf096294faba49e4d664109a
Mingjie Jiang <jiang@mingjie.info>
2019-04-10 18:18:34 -0400

    Clean up unused assets

---
+++ docs/_assets/js/app.js
@@ -0,0 +1,26 @@
+/** Initialize Docsify */
+window.$docsify = {
+  name: "Leadership Guide",
+  logo: "/_media/branding/favicon.svg",
+  loadSidebar: true,
+  maxLevel: 3,
+  subMaxLevel: 2,
+  auto2top: true,
+  search: {
+    placeholder: "Type to search",
+    noData: "No results!",
+    depth: 2,
+    hideOtherSidebarContent: true
+  },
+  footer: {
+    text: "Contribute to This Page",
+    repo: "hackclub/guide",
+    cssClass: "footer",
+    custom: `Made with 💖 by <a href="https://github.com/itsmingjie">@itsmingjie</a>.`
+  }
+};
+
+/** Register Service Worker */
+if (typeof navigator.serviceWorker !== "undefined") {
+  navigator.serviceWorker.register("sw.js");
+}
